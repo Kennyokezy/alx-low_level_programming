2cf562042105beedd0a739bbe3f5d0d9a0d257b2
Kennyokezy <ostephenkenny@gmail.com>
2022-06-30 16:48:09 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  * Return: returns 1
  **/
 int main(void)
-{char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19/n";
-	write(2, ch, sizeof(ch));
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"61);
 	return (1);
 }
