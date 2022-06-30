dcdf5a69b5f91770f181a165ee4419f66856a7fb
Kennyokezy <ostephenkenny@gmail.com>
2022-06-30 16:36:59 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - prints the standard error
- * Returns: returns 1
+ * Return: returns 1
  **/
 int main(void)
 {char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19/n";
-	write(2, ch, sizeof(ch))
+	write(2, ch, sizeof(ch));
 	return (1);
 }
