ee934060dcdefc04a883124ba3443f239cd8cc10
Kennyokezy <ostephenkenny@gmail.com>
2022-06-30 19:26:16 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n”
-int len ;
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n”, 61);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 	return (1);
 }
