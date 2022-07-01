d8711247fe8130e903e919b99436b22b51b57f56
Kennyokezy <ostephenkenny@gmail.com>
2022-06-30 19:42:22 -0700

    quote bullshit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	ssize_t write(int fd, const void *buf, size_t count);
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
