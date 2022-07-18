d48ce06ce6c811f615879a9183aaa07a1b7b09c5
Kennyokezy <ostephenkenny@gmail.com>
2022-07-18 16:36:08 -0700

    getting some memes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- *memset - Fills the first n bytes of the memory area
- *pointed to by @s with the constant byte @c.
- *@s: A pointer to the memory area to be filled.
- *@c: The character to fill the memory area with.
- *@n: The number of bytes to be filled.
- *Return: A pointer to the filled memory area @s.
+ * *_memset - fills memory with a constant byte
+ * @n: # of bytes to be filled
+ * @b: value of the integer
+ * @s: memory area
+ * Return: s
  */
-void *_memset(void *s, int c, size_t n)
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
-
-	for (index = 0; index < n; index++)
-	memory[index] = value;
-
-	return (memory);
+	while (n > 0)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
